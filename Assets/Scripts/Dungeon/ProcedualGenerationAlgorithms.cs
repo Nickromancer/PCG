ee23b2f5c0aeacf608ce838fe3f6499fb4a06149
Nicholas Hansen <Nicholas.Hansen101@gmail.com>
2026-01-03 18:07:10 +0100

    Add CorridorFirst Algorithm

--- Assets/Scripts/Dungeon/ProcedualGenerationAlgorithms.cs
+++ Assets/Scripts/Dungeon/ProcedualGenerationAlgorithms.cs
@@ -6,9 +6,10 @@ public static class ProcedualGenerationAlgorithms
 {
     public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
     {
-        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
-
-        path.Add(startPosition);
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>
+        {
+            startPosition
+        };
         var previousPosition = startPosition;
 
         for (int i = 0; i < walkLength; i++)
@@ -18,6 +19,20 @@ public static class ProcedualGenerationAlgorithms
             previousPosition = newPosition;
         }
         return path;
+    }
+
+    public static List<Vector2Int> RandomWalkCorridor(Vector2Int startPosition, int corridorLength)
+    {
+        List<Vector2Int> corridor = new List<Vector2Int>();
+        var direction = Direction2D.GetRandomCardinalDirection();
+        var currentPosition = startPosition;
+        corridor.Add(currentPosition);
+        for (int i = 0; i < corridorLength; i++)
+        {
+            currentPosition += direction;
+            corridor.Add(currentPosition);
+        }
+        return corridor;
     }
 }
 
